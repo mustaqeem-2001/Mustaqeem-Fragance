bf2d2484cc88e88a607316fed96f5cfaa87bee24
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2025-10-25 19:41:04 +0100

    Added functionality of ittarss correctly showing, and within page containers that are limited to 9 each. Additionally added the contact us text at the bottom. And done some styling to them.

--- data.js
+++ data.js
@@ -2,101 +2,121 @@ export const ittarData = [
     {
         name: "Gold Sandal",
         price: 15,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Bakhoor Cudimusk",
         price: 15,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "One Million Royal Men",
         price: 20,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Amber Oud",
         price: 15,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Secret Men",
         price: 15,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Oud Cambodie",
         price: 30,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Bukhari Oud",
         price: 15,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Sundul Rose",
         price: 10,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Musk-E-Tahara",
         price: 10,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Musk-Ul-Rose",
         price: 10,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Royal Musk",
         price: 10,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Secret Men",
         price: 10,
-        size: "3ML"
+        size: "3ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Arabic Oud",
         price: 20,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Aseel Attar",
         price: 15,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Black Musk",
         price: 15,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Hajre Aswad",
         price: 15,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "1001 Nights",
         price: 15,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Musk Rose SPL",
         price: 20,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Musk Al Rizali",
         price: 15,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     },
     {
         name: "Ruh Khus",
         price: 30,
-        size: "6ML"
+        size: "6ML",
+        image: "ittar.jpeg"
     }
 ]
--- index.js
+++ index.js
@@ -1,3 +1,57 @@
 import { ittarData } from '/data.js';
 
-console.log(ittarData);
+
+const itemListsEl = document.getElementById("itemLists");
+const pageNumberEl = document.getElementById("pageListContainer");
+
+let pageCount = 0;
+let maxperPage = 9;
+
+
+pageNumberEl.addEventListener("click", function(e) {
+    console.log(e.target.id);
+    if (e.target.id && e.target.classList.contains("page-list-container")) {
+        console.error("Did not click a page number.")
+    }
+    else if(e.target.id) {
+        pageSections(e.target.id.replace("page", ''));
+    }
+    else {
+        console.error("Empty Id");
+    }
+})
+
+function render(array) {
+    const itemListsHTML = array.map(function(ittar) {
+        return `
+        <div>
+            <img src="/images/${ittar.image}"/>
+            <p>${ittar.name} <span>${ittar.size}</span></p>
+            <p>£${ittar.price}</p>
+        </div>
+        `
+    }).join('');
+    itemListsEl.innerHTML = itemListsHTML;
+}
+
+function totalPageCount() {
+    let pageNumberHTML = ``;
+    while (pageCount < ittarData.length / 9) {
+        pageCount++;
+        pageNumberHTML += `
+        <div class="page-number-container" id="page${pageCount}">
+            ${pageCount}
+        </div>
+        `
+    }
+    pageNumberEl.innerHTML = pageNumberHTML;
+}
+
+function pageSections(pageNumber = 1) {
+    const startIndex = (pageNumber - 1) * maxperPage;
+    const endIndex = startIndex + maxperPage;
+    const pageData = ittarData.slice(startIndex, endIndex);
+    render(pageData);
+}
+pageSections();
+totalPageCount();
